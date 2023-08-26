d81f75aecb26585710606bd7a226deab651eeb6d
Client <client@Clients-MacBook-Pro.local>
2023-08-26 19:09:32 +0100

    my commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+"""Determine the fewest number of coins needed
+to meet a given amount total"""
+
 
 def makeChange(coins, total):
+    '''This function takes in a list(coins) and counts it off of total'''
     if total <= 0:
         return 0
 
-    # Create a list to store the minimum number of coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Calculate the minimum number of coins for each amount
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
-
+    else:
+        count = 0
+        sorted_coin = sorted(coins)
+        sorted_coin.reverse()
+        for coin in sorted_coin:
+            while (total >= coin):
+                total -= coin
+                count += 1
+        if total == 0:
+            return count
+        return -1
