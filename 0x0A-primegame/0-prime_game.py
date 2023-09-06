58a368196d9416753dac55f73d01e3fff452c4a6
Client <client@Clients-MacBook-Pro.local>
2023-09-06 15:01:01 +0100

    my commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def playGame(n):
+    # Implement game logic for a single round here
+    # Return the winner (Maria or Ben)
+
+def isWinner(x, nums):
+    maria_wins = 0
+    ben_wins = 0
+
+    # Loop through each round
+    for n in nums:
+        winner = playGame(n)
+
